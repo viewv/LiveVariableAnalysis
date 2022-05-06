6a8958ec3c1ad1249dbfc62eb5d17c303b9ff828
viewv <zxnnet@gmail.com>
2022-05-06 17:31:42 +0800

    Fix one comment

--- src/main/java/top/viewv/ValueMap.java
+++ src/main/java/top/viewv/ValueMap.java
@@ -11,7 +11,7 @@ public class ValueMap extends HashMap<Value, Constant> {
         if (super.containsKey(key)){
             return super.get(key);
         }else {
-            //如果不存在，则创建一个新的 Constant 对象
+            //如果不存在，则返回一个新的 Constant 对象
             if (key instanceof IntConstant){
                 return new Constant(((IntConstant) key).value);
             }
